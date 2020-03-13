4a9fae67265d73eb89e9ed0efb03f17ecb06797c
maja26 <43316150+maja26@users.noreply.github.com>
2020-03-13 08:09:13 +0100

    projekt angelegt

---
+++ findMyBanana_projekt/findMyBanana_projekt/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  findMyBanana_projekt
+//
+//  Created by Maja Drinovac on 13.03.20.
+//  Copyright © 2020 Maja Drinovac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ findMyBanana_projekt/findMyBanana_projektTests/findMyBanana_projektTests.swift
@@ -0,0 +1,34 @@
+//
+//  findMyBanana_projektTests.swift
+//  findMyBanana_projektTests
+//
+//  Created by Maja Drinovac on 13.03.20.
+//  Copyright © 2020 Maja Drinovac. All rights reserved.
+//
+
+import XCTest
+@testable import findMyBanana_projekt
+
+class findMyBanana_projektTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
