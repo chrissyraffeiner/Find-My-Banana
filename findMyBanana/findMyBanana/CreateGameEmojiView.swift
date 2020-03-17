44ae200c21060f66466aad823f07c08a29724d54
Laura <laura.riener02@icloud.com>
2020-03-17 14:43:33 +0100

    anz emojis korrigiert

---
+++ findMyBanana/findMyBanana/CreateGameEmojiView.swift
@@ -0,0 +1,60 @@
+//
+//  CreateGameEmojiView.swift
+//  findMyBanana
+//
+//  Created by Administrator on 13.03.20.
+//  Copyright © 2020 Laura Riener. All rights reserved.
+//
+
+import UIKit
+
+class CreateGameEmojiView:  UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+
+    @IBOutlet weak var emojiPicker: UIPickerView!
+    var selected : Int = -1
+    
+    @IBOutlet weak var testLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        self.emojiPicker.delegate = self
+        self.emojiPicker.dataSource = self
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return 7-2
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return "\(row + 3) emojis"
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        print(row)
+        selected = row + 3
+        print("\(selected)")
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ findMyBanana/findMyBanana/CreateGameTimeView.swift
@@ -0,0 +1,60 @@
+//
+//  CreateGameTimeView.swift
+//  findMyBanana
+//
+//  Created by Administrator on 13.03.20.
+//  Copyright © 2020 Laura Riener. All rights reserved.
+//
+
+import UIKit
+
+class CreateGameTimeView: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    @IBOutlet weak var timePicker: UIPickerView!
+    var selected : Int = -1
+    
+    @IBOutlet weak var testLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        self.timePicker.delegate = self
+        self.timePicker.dataSource = self
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return 30-4
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return "\(row + 5) sec"
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        print(row)
+        selected = row + 5
+        print("\(selected)")
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
