1455bd974bdd6c05bde64251e536592610ac9519
Raffeiner Christine <44089520+Chruss8@users.noreply.github.com>
2020-03-20 11:56:57 +0100

    StartView fertig inclusive seque

--- findMyBanana/findMyBanana/ViewController.swift
+++ findMyBanana/findMyBanana/ViewController.swift
@@ -1,20 +1,30 @@
-//
-//  ViewController.swift
-//  findMyBanana
-//
-//  Created by Administrator on 13.03.20.
-//  Copyright © 2020 Laura Riener. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var findmxBananaView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func ConnectGame(_ sender: Any) {
+         performSegue(withIdentifier: "showJoinGameSence1", sender: self)
+        
+    }
+    
+    @IBAction func CreateGame(_ sender: Any) {
+         performSegue(withIdentifier: "showCreateGame1", sender: self)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if(segue.identifier == "showCreateGame1"){
+            
+        }
+        
+        if(segue.identifier == "showJoinGameSence1"){
+            
+        }
+    }
 }
-
