fc5f3abffc313219a7c66b218b420026aeb8f267
maja26 <43316150+maja26@users.noreply.github.com>
2020-04-29 19:27:46 +0200

    change ip

--- findMyBanana/findMyBanana/CreateGameGamecodeView.swift
+++ findMyBanana/findMyBanana/CreateGameGamecodeView.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class CreateGameGamecodeView: UIViewController {
-    let createGameUrl = "http://192.168.1.104:3000/createGame"
+    let createGameUrl = "http://192.168.0.100:3000/createGame"
     //let createGameUrl = "http://127.0.0.1:3000/createGame"
     var token = ""
     var jsonModel = GameModel(anz: 3, timeInSec: 5)
